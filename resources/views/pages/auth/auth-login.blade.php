29156330ea79fcaab31f6037fbfa11bc0e89fc53
Suryawan Hidayat <hidayat.suryawan@gmail.com>
2023-09-20 21:12:32 +0700

    fix week 2

--- resources/views/pages/auth/auth-login.blade.php
+++ resources/views/pages/auth/auth-login.blade.php
@@ -28,7 +28,7 @@
                     <div class="d-block">
                         <label for="password" class="control-label">Password</label>
                         <div class="float-right">
-                            <a href="auth-forgot-password.html" class="text-small">
+                            <a href="forgot" class="text-small">
                                 Forgot Password?
                             </a>
                         </div>
