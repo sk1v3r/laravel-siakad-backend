c916a50996d81b64483e9081ad797b6cd79e69ff
Suryawan Hidayat <hidayat.suryawan@gmail.com>
2023-10-01 07:17:29 +0700

    setting api

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\ScheduleController;
+use App\Models\Schedule;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use Laravel\Sanctum\Sanctum;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +22,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+Route::apiResource('schedules', ScheduleController::class)->middleware('auth:sanctum');
+// Route::apiResource('schedules', ScheduleController::class)
+//     ->middleware('auth:sanctum');
